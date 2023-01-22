4b5ab819980e4acb69e267e7cf69f6027d21589e
sas229 <sas229@cam.ac.uk>
2023-01-22 11:02:55 +0000

    Some documentation edits.

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version="0.0.1",
     description="A PIV/DIC analysis package for Python.",
     author="Sam Stanier",
-    license="GNU GPLv3",
     packages=["geopyv", "geopyv.gui", "geopyv.geometry"],
     package_dir={"": "src"},
     cmake_install_dir="src/geopyv",

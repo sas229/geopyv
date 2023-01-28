22ad72f889431d927c1f8ac1c124c55fa6e4b9d2
sas229 <sas229@cam.ac.uk>
2023-01-28 19:59:32 +0000

    Adding build_wheel.yml workflow.

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(
     packages=["geopyv", "geopyv.geometry", "geopyv.gui", "geopyv.gui.selectors"],
     package_dir={"": "src"},
     cmake_install_dir="src/geopyv",
+    include_package_data=True,
 )
